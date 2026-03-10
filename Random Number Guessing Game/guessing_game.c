de326e27f0b7c60be7b42b12d45f697bd8c822ff
Shambhavi <120776110+shaambhaviii@users.noreply.github.com>
2026-03-10 16:38:55 +0530

    Update guessing_game.c

--- Random Number Guessing Game/guessing_game.c
+++ Random Number Guessing Game/guessing_game.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //for seeding
 
+int main(){
+    int random,guess;
+    int no_of_guess = 0;
+    srand(time(NULL)); //seeding function, so on the basis of current time srand will be seed 
+    printf("Welcome to World of Guessing Numbers");
+    printf("(This is a small part of shamv's universe)");
+    random = rand() %100 +1; //rand generated a decimal number between 0 to 1 (in java)
+    // in c we will be gpoing for mod func as quotient can be large but remainder will be small.
+    //generating between 0 to 100
+    // printf("number generated %d",random);
+
+    do {
+        printf("\nPlease enter your Guess between (1 to 100):");
+        scanf("%d",&guess);
+        no_of_guess++;
+        
+        if(guess<random){
+            printf("Guess larger number.\n");
+        }
+        else if(guess>random){
+            printf("Guess smaller number.\n");
+        }
+        else{
+            printf("Congratulations!!! You have guessed the Number in %d attempts",no_of_guess);
+        }
+        
+    }while(guess!=random);
+    printf("\nBye Bye Thanks for playing");
+    printf("\nDeveloped by Shamv:)");
+
+    return 0;
+}
